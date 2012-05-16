c8b114ac25df19d4e8de14ba2e8d1f23ce00340b
unknown <jml00@JOSHL-LT.AcuityLightingGroup.com>
2012-05-16 14:02:57 -0400

    Fixed model so hashtables are used to create different piece objects in chessboard object.

--- chess.js
+++ chess.js
@@ -1,27 +1,62 @@
-/**
- * @author Josh
- */
+// Created by Josh
+
+$(document).ready(function(){
+   
+   var newBoard = InitBoard();
+
+   
+   $('.piece').draggable({
+	
+   });
+   
+   $('.space').droppable({
+			hoverClass: 'piece-hover',
+			drop: function(event, ui) {
+				$(this).addClass("piece-dropped");
+			}
+   });
+   
+});
+   
 
 function InitBoard()
 {
 	// init pieces to start
-	var whiteKing = {type: 'king', color: 'white', value: 100};
-	var blackKing = {type: 'king', color: 'black', value: 100};
+	var king = {};
+	king['white'] = {type: 'king', color: 'white', value: 100};
+	king['black'] = {type: 'king', color: 'black', value: 100};
 	
-	var whiteQueen = {type: 'queen', color: 'white', value: 10};
-	var blackQueen = {type: 'queen', color: 'black', value: 10};
+	var queen = {};
+	queen['white'] = {type: 'queen', color: 'white', value: 9};
+	queen['black'] = {type: 'queen', color: 'black', value: 9};
 	
-	var whiteRook = {type: 'rook', color: 'white', value: 5};
-	var blackRook = {type: 'rook', color: 'black', value: 5};
+	var rook = {};
+	rook['white1'] = {type: 'rook', color: 'white', value: 5};
+	rook['white2'] = {type: 'rook', color: 'white', value: 5};
+	rook['black1'] = {type: 'rook', color: 'black', value: 5};
+	rook['black2'] = {type: 'rook', color: 'black', value: 5};
 	
-	var whiteKnight = {type: 'knight', color: 'white', value: 3};
-	var blackKnight = {type: 'knight', color: 'black', value: 3};
+	var knight = {};
+	knight['white1'] = {type: 'knight', color: 'white', value: 3};
+	knight['white2'] = {type: 'knight', color: 'white', value: 3};
+	knight['black1'] = {type: 'knight', color: 'black', value: 3};
+	knight['black2'] = {type: 'knight', color: 'black', value: 3};
 	
-	var whiteBishop = {type: 'bishop', color: 'white', value: 3};
-	var blackBishop = {type: 'bishop', color: 'black', value: 3};
 	
-	var whitePawn = {type: 'pawn', color: 'white', value: 1};
-	var blackPawn = {type: 'pawn', color: 'black', value: 1};
+	var bishop = {};
+	bishop['white1'] = {type: 'bishop', color: 'white', value: 3};
+	bishop['white2'] = {type: 'bishop', color: 'white', value: 3};
+	bishop['black1'] = {type: 'bishop', color: 'black', value: 3};
+	bishop['black2'] = {type: 'bishop', color: 'black', value: 3};
+	
+	var pawn = {};
+	for (i=1; i<9; i++)
+	{
+		var whitePawnName = 'white' + i;
+		var blackPawnName = 'black' + i;
+		pawn[whitePawnName] = {type: 'pawn', color: 'white', value: 1};
+		pawn[blackPawnName] = {type: 'pawn', color: 'black', value: 1};
+	}
 
 	var chessBoard = [];
 	
@@ -31,26 +66,34 @@ function InitBoard()
 		chessBoard[i] = [];
 	}
 	
-	for (i=0; i<8; i++)
+	for (i=1; i<9; i++)
 	{
-		chessBoard[1][i] = blackPawn;
-		chessBoard[6][i] = whitePawn;				
+		var whitePawnName = 'white' + i;
+		var blackPawnName = 'black' + i;
+		chessBoard[1][i-1] = pawn[whitePawnName];
+		chessBoard[6][i-1] = pawn[blackPawnName];				
 	}
 
-	chessBoard[0][0], chessBoard[0][7] = whiteRook;
-	chessBoard[7][0], chessBoard[7][7] = blackRook;
+	chessBoard[0][0] = rook['white1'];
+	chessBoard[0][7] = rook['white2'];
+	chessBoard[7][0] = rook['black1'];
+	chessBoard[7][7] = rook['black2'];
 				
-	chessBoard[0][1], chessBoard[0, 6] = whiteKnight;
-	chessBoard[7][1], chessBoard[7, 6] = blackKnight;
+	chessBoard[0][1] = knight['white1'];
+	chessBoard[0][6] = knight['white2'];
+	chessBoard[7][1] = knight['black1'];
+	chessBoard[7][6] = knight['black2'];
 				
-	chessBoard[0][2], chessBoard[0, 5] = whiteBishop;
-	chessBoard[7][2], chessBoard[7, 5] = blackBishop;
+	chessBoard[0][2] = bishop['white1'];
+	chessBoard[0][5] = bishop['white2'];
+	chessBoard[7][2] = bishop['black1'];
+	chessBoard[7][5] = bishop['black2'];
 				
-	chessBoard[0][3] = whiteQueen;
-	chessBoard[7][3] = blackQueen;
+	chessBoard[0][3] = queen['white'];
+	chessBoard[7][3] = queen['black'];
 				
-	chessBoard[0][4] = whiteKing;
-	chessBoard[7][4] = blackKing;
+	chessBoard[0][4] = king['white'];
+	chessBoard[7][4] = king['black'];
 
 	return chessBoard;
 }
@@ -93,23 +136,3 @@ function DetermineLegalMoves(piece, location)
 	
 	
 }
-
-
- $(document).ready(function(){
-   
-   var newBoard = InitBoard();
-
-   
-   $('.piece').draggable({
-	
-   });
-   
-   $('.space').droppable({
-			hoverClass: 'piece-hover',
-			drop: function(event, ui) {
-				$(this).addClass("piece-dropped");
-			}
-   });
-   
-});
-   
